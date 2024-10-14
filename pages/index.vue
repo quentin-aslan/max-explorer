5b17ee9531bebdc1b4c09f16bc48a75ff4540f2d
Aslan <quentin.aslan@outlook.com>
2024-10-15 00:22:06 +0200

    :sparkles: Build Fixed, Prisma Nuxt Module disable on Nuxt dev tools. + daisyUI removed.

--- pages/index.vue
+++ pages/index.vue
@@ -9,12 +9,12 @@
     <!-- Section texte -->
     <div class="order-1 lg:order-2 lg:w-2/3 flex lg:justify-start">
       <div class="text-center flex flex-col justify-between lg:text-left p-6 lg:p-12">
-        <h1 class="text-3xl lg:text-5xl font-bold text-surface-900 dark:text-surface-0 mb-4">
+        <h1 class="text-3xl lg:text-5xl font-bold text-surface-900 mb-4">
           Découvre la France 🇫🇷
           <br />
-          <span class="text-blue-800 dark:text-blue-400">Voyage avec MAX JEUNE</span>
+          <span class="text-blue-800">Voyage avec MAX JEUNE</span>
         </h1>
-        <p class="text-lg text-surface-700 dark:text-surface-200 leading-relaxed">
+        <p class="text-lg text-surface-700 leading-relaxed">
           Profite de trajets gratuits et illimités avec MAX JEUNE ! Pars à l'aventure depuis ta ville en quelques clics.
           <br />
           Pas besoin d'une destination précise ? Renseigne simplement ta ville de départ et tes dates, et on s'occupe du reste !
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
   ],
 }
 
