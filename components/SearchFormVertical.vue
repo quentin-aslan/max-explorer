d992037a93f5757b1b51ceec26ae0412f934f88d
Aslan <quentin.aslan@outlook.com>
2024-10-17 23:01:49 +0200

    :sparkles: DatePicker ux improved (dateMin and dateMax implemented) 📅 + Homepage Improved. + favicon added.

--- components/SearchFormVertical.vue
+++ components/SearchFormVertical.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit.prevent="research" class="flex flex-col gap-4 w-full bg-white shadow-md rounded-md p-6">
-    <h2 class="text-2xl text-blue-800 font-bold mb-4">Recherche ta prochaine destination !</h2>
+    <h2 class="text-2xl text-blue-900 font-bold mb-4">Recherche ta prochaine destination !</h2>
 
     <div>
       <label class="block text-gray-700 mb-2" for="departureStation">Ville de départ</label>
@@ -14,12 +14,12 @@
 
     <div>
       <label class="block text-gray-700 mb-2" for="departureDate">Date de départ</label>
-      <Calendar v-model="departureDate" :showIcon="true" dateFormat="dd/mm/yy" class="w-full"/>
+      <DatePicker v-model="departureDate" :minDate="departureDateMin" :maxDate="dateMax" :showIcon="true" dateFormat="dd/mm/yy" class="w-full"/>
     </div>
 
     <div>
       <label class="block text-gray-700 mb-2" for="returnDate">Date de retour</label>
-      <Calendar v-model="returnDate" :showIcon="true" dateFormat="dd/mm/yy" class="w-full"/>
+      <DatePicker v-model="returnDate" :minDate="returnDateMin" :maxDate="dateMax"  :showIcon="true" dateFormat="dd/mm/yy" class="w-full"/>
     </div>
 
     <Button type="submit" label="Rechercher !" class="w-full"/>
@@ -28,4 +28,9 @@
 
 <script setup>
 const { departureStation, destinationStation, departureDate, returnDate, research } = useSearchForm()
+
+const departureDateMin = ref(new Date())
+const returnDateMin = computed (() => departureDate.value ?? departureDateMin.value)
+// 30 day from now because SNCF API only allows 30 days in the future
+const dateMax = ref(new Date(new Date().setDate(new Date().getDate() + 30)));
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,53 @@
 <template>
-  <div class="md:p-15 flex flex-col justify-center lg:items-center lg:flex-row lg:gap-8 p-6">
-    <!-- Barre de recherche sur la gauche en mode desktop -->
-    <div class="order-2 lg:order-1 lg:w-1/3 flex justify-center lg:justify-start">
-      <SearchFormVertical />
+  <div class="min-h-screen flex flex-col justify-center lg:items-center lg:flex-row lg:gap-8 p-6 md:p-15 bg-gray-50">
+    <!-- Section texte avec les étapes -->
+    <div class="order-1 lg:order-2 lg:w-3/5 flex lg:justify-start">
+      <div class="flex flex-col gap-4 lg:text-left">
+        <!-- Logo MaxJeune avec attribut alt descriptif -->
+        <NuxtImg
+            src="/brand-logo-max-junior.svg"
+            alt="Logo de l'abonnement MAX JEUNE"
+            class="w-40"
+        />
+
+        <!-- Utilisation appropriée des balises de titre -->
+        <h1 class="hidden lg:block text-xl lg:text-3xl font-bold text-gray-900">
+          Découvrez de nouvelles destinations 🇫🇷
+        </h1>
+
+          <!-- Formulaire de recherche en version mobile -->
+          <SearchFormVertical v-if="isMobile" />
+
+        <!-- Présentation des étapes avec balises de liste pour une meilleure sémantique -->
+        <ol class="flex flex-col lg:gap-4 list-decimal list-inside">
+          <li class="flex items-center mb-4 lg:mb-0 lg:mr-8">
+            <i class="pi pi-map-marker text-blue-900 text-3xl mr-2"></i>
+            <span class="text-lg">1. Indique ta ville de départ</span>
+          </li>
+          <li class="flex items-center mb-4 lg:mb-0 lg:mr-8">
+            <i class="pi pi-calendar text-blue-900 text-3xl mr-2"></i>
+            <span class="text-lg">2. Choisis tes dates</span>
+          </li>
+          <li class="flex items-center">
+            <i class="pi pi-compass text-blue-900 text-3xl mr-2"></i>
+            <span class="text-lg">3. Découvre où tu peux aller à 0€</span>
+          </li>
+        </ol>
+
+        <!-- Ajout d'un paragraphe descriptif pour enrichir le contenu -->
+        <p class="text-lg">
+          Utilisez notre application pour tirer le meilleur parti de votre abonnement <strong>MAX JEUNE</strong>. Découvrez toutes les destinations accessibles depuis votre ville de départ en quelques clics !
+        </p>
+
+      </div>
     </div>
 
-    <!-- Section texte -->
-    <div class="order-1 lg:order-2 lg:w-2/3 flex lg:justify-start">
-      <div class="flex flex-col justify-between lg:text-left p-6 lg:p-12">
-        <h1 class="text-2xl lg:text-5xl font-bold text-surface-900 mb-4">
-          Découvre la France 🇫🇷 <br> <span class="text-blue-800">avec MAX JEUNE</span>
-          <br />
-        </h1>
-        <p class="text-lg text-surface-700 leading-relaxed">
-          <span>Renseigne simplement ta ville de départ et tes dates et on te propose la liste des destinations eligible <span class="text-blue-900 font-bold">MAX JEUNE à <span class="text-2xl">0€</span></span> !</span>
-        </p>
-      </div>
+    <!-- Barre de recherche sur la gauche en mode desktop -->
+    <div v-if="!isMobile" class="order-2 lg:order-1 lg:w-2/5 lg:flex justify-center lg:justify-start">
+      <SearchFormVertical />
     </div>
   </div>
 </template>
+<script setup lang="ts">
+const { isMobile } = useIsMobile()
+</script>
